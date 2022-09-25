e3042de96a93eea11b685f3d0d81bb9fc34f9307
Marek <364021318@qq.com>
2022-09-25 22:47:01 +0800

    fix: package import url

--- main.go
+++ main.go
@@ -53,8 +53,8 @@ import (
 	"os"
 
 	"github.com/golang/protobuf/proto"
-	"github.com/micro/protoc-gen-micro/v2/generator"
-	_ "github.com/micro/protoc-gen-micro/v2/plugin/micro"
+	"github.com/Marek-maxs/protoc-gen-micro/v2/generator"
+	_ "github.com/Marek-maxs/protoc-gen-micro/v2/plugin/micro"
 )
 
 func main() {
